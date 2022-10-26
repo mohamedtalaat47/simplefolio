e363ab55717be5648feb77b7ec8f7628b6c02ba6
Mohamed Talaat <mohamed_talaat_47@yahoo.com>
2022-10-26 08:21:10 +0200

    preload animation added

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+
+const myPreloader = document.querySelector('.preloader');
+
+const fadeOutEffect = setInterval(() => {
+    if (!myPreloader.style.opacity) {
+        myPreloader.style.opacity = 1;
+    }
+    if (myPreloader.style.opacity > 0) {
+        myPreloader.style.opacity -= 0.1;
+    } else {
+        clearInterval(fadeEffect);
+    }
+}, 300);
+
+window.addEventListener('load', fadeOutEffect);
